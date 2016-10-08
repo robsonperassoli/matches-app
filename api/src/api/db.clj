81def12a3be982acb7f40191400532a9f802de39
Robson Gian Perassoli <robsonperassoli@gmail.com>
2016-10-08 15:14:04 -0300

    remove api

--- api/src/api/db.clj
+++
@@ -1,4 +0,0 @@
-(ns api.db
-  (:require [monger.core :as mg]))
-
-(def db (mg/get-db (mg/connect) "soccer-mgr"))
--- api/src/api/teams.clj
+++
@@ -1,5 +0,0 @@
-(ns api.teams
-  (:require [clj-json.core :as json]))
-
-(defn list []
-  (json/parse-string (slurp "resources/teams.json")))
